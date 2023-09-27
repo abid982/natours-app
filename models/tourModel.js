5bd606c97ec823edab4352fea9530dfd51bb90fa
abid982 <abidarif982@gmail.com>
2023-09-27 20:39:42 +0500

    Implement document middleware to create slug of each tour before save document to the database

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // We pass in not only the object with the schema definition itself but also an object for the schema options
 const tourSchema = new mongoose.Schema(
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -58,7 +60,7 @@ const tourSchema = new mongoose.Schema(
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  }
+  },
 );
 
 // Define virtual properties on the Tour Schema tourSchema.virtual() and then pass the name of the virtual property and then on there we need to define the get method because this virtual property here will basically be created each time that we get some data out of the database so this get function here is called a getter
@@ -71,6 +73,64 @@ tourSchema.virtual('durationWeeks').get(function () {
   // Convert days to weeks
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: It runs before save() and create() and not on insertMany() it's not triggered the save middleware
+// It's going to run before an actual event and that event in this case is the save event
+// Pass callback function and this callback function is going to run before an acutal document is saved to the database.
+
+// Create a new document using our API in order to trigger this document middleware
+// Call next middleware
+tourSchema.pre('save', function (next) {
+  console.log('Document pre middleware:');
+  // Currently processed document that is being saved
+  // This is what our document is looking like right before it's saved into the database
+  /*
+    Document middleware:
+{
+  name: 'Test Tour',
+  duration: 1,
+  maxGroupSize: 1,
+  difficulty: 'difficult',
+  ratingsAverage: 4.5,
+  ratingsQuantity: 0,
+  price: 1,
+  summary: 'Test tour',
+  imageCover: 'tour-7-cover.jpg',
+  images: [],
+  createdAt: 2023-09-27T08:36:54.611Z,
+  startDates: [],
+  _id: new ObjectId("6513e9f7d606616218af3cf3"),
+  durationWeeks: 0.14285714285714285,
+  id: '6513e9f7d606616218af3cf3'
+}
+
+At this point we still act on the data before it then saved to the database and that's exactly what we're going to do now. What we want to do here is to create a slug for each of these documents.
+Install slugify package
+  */
+  console.log(this);
+  // Define a new property on currently processed document
+  // Note: Define slug in Schema first otherwise it's not going to be persisted in the database.
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+// // We can create multiple pre middleware
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+
+//   next();
+// });
+
+// // Let's now just very quickly experiment also with a post middleware
+// // The post middleware as access not only to next but also to the document that was just saved to the database
+// // Middleware or Hook | Post Save Hook
+// tourSchema.post('save', function (document, next) {
+//   // We don't access to the this keyword but the finished document
+//   console.log('Document post middleware:');
+//   console.log(document);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
