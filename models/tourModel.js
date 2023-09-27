3f47cbd7815bf49d18607f9f42da51a40bff249e
abid982 <abidarif982@gmail.com>
2023-09-27 12:35:48 +0500

    Add virtual property durationWeeks in order to convert duration from days to weeks

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,75 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
+// We pass in not only the object with the schema definition itself but also an object for the schema options
+const tourSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
     },
     duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration']
+      type: Number,
+      required: [true, 'A tour must have a duration'],
     },
     maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size']
+      type: Number,
+      required: [true, 'A tour must have a group size'],
     },
     difficulty: {
-        type: String,
-        required: [true, 'A string mush have a difficulty']
+      type: String,
+      required: [true, 'A string mush have a difficulty'],
     },
     ratingsAverage: {
-        type: Number,
-        default: 4.5
+      type: Number,
+      default: 4.5,
     },
     ratingsQuantity: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
+      type: Number,
+      required: [true, 'A tour must have a price'],
     },
     priceDiscount: Number,
     summary: {
-        type: String,
-        trim: true,
-        required: [true, 'A tour must have a description']
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
     },
     description: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image']
+      type: String,
+      required: [true, 'A tour must have a cover image'],
     },
     images: [String],
     createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false
+      type: Date,
+      default: Date.now(),
+      select: false,
     },
-    startDates: [Date]
+    startDates: [Date],
+  },
+  //   Each time that the data is actually outputted as json we want virtuals to be true so basically the virtual to be the part of the output.
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Define virtual properties on the Tour Schema tourSchema.virtual() and then pass the name of the virtual property and then on there we need to define the get method because this virtual property here will basically be created each time that we get some data out of the database so this get function here is called a getter
+// Now in here we pass a function and actually this callback function is going to be a real function so not an arrow function because the arrow function does not get its own this keyword but we need the this keyword.
+// The virtual property durationWeeks doen't persited in the database but it's only going to be there as soon as we get the data.
+// We also need to explicitly define the virtual properties in our schema
+tourSchema.virtual('durationWeeks').get(function () {
+  // Define a virtual property
+  // The this keywork points to the current document
+  // Convert days to weeks
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
